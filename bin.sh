55d0a99369530d38e8d5e8eb45f5971ade928605
binge8 <53508344+binge8@users.noreply.github.com>
2021-03-19 19:25:24 +0800

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -10,12 +10,7 @@ sed -i '92d' package/system/opkg/Makefile
 
 #添加额外软件包
 git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/openwrt-packages/brook
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/openwrt-packages/chinadns-ng
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/openwrt-packages/trojan-go
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/openwrt-packages/trojan-plus
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/openwrt-packages/ssocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/openwrt-packages/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-packages/luci-app-passwall
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-autopoweroff package/openwrt-packages/luci-app-autopoweroff
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-control-timewol package/openwrt-packages/luci-app-control-timewol
 git clone https://github.com/tty228/luci-app-serverchan.git package/openwrt-packages/luci-app-serverchan
@@ -26,7 +21,7 @@ git clone https://github.com/binge8/luci-theme-butongwifi.git package/openwrt-pa
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/openwrt-packages/luci-theme-atmaterial
 git clone https://github.com/binge8/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
 git clone https://github.com/binge8/luci-app-koolddns.git package/openwrt-packages/luci-app-koolddns
-git clone https://github.com/tuanqing/install-program package/openwrt-packages/install-program
+#git clone https://github.com/tuanqing/install-program package/openwrt-packages/install-program
 svn co https://github.com/0saga0/OpenClash/trunk/luci-app-openclash package/openwrt-packages/luci-app-openclash
 svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/openwrt-packages/luci-theme-infinityfreedom
 
