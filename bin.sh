4b0c1664dd993ec2b9e6732c98560eef6d7f9e7b
binge8 <53508344+binge8@users.noreply.github.com>
2021-06-14 21:31:26 +0800

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -23,11 +23,7 @@ git clone https://github.com/binge8/luci-app-koolddns.git package/openwrt-packag
 #git clone https://github.com/tuanqing/install-program package/openwrt-packages/install-program
 svn co https://github.com/0saga0/OpenClash/trunk/luci-app-openclash package/openwrt-packages/luci-app-openclash
 svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/openwrt-packages/luci-theme-infinityfreedom
-
-#dockerman
 git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
-sed -i '/dockerd/d' package/openwrt-packages/luci-app-dockerman/applications/luci-app-dockerman/Makefile
-sed -i 's/+docker/+docker-ce/g' package/openwrt-packages/luci-app-dockerman/applications/luci-app-dockerman/Makefile
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
