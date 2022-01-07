f8de4be8aa5f9678a0add3bc085e30864ff0c567
binge8 <53508344+binge8@users.noreply.github.com>
2022-01-07 22:20:46 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -14,3 +14,8 @@ git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packa
 #更新安装feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+#设置环境变量
+useVersionInfo=$(git show -s --date=short --format="编译前的最后一次[➦主源码](https://github.com/coolsnowwolf/lede)更新记录:<br/>更新人: %an<br/>更新时间: %cd<br/>更新内容: %s<br/>哈希值: %H")
+echo "useVersionInfo=$useVersionInfo" >> $GITHUB_ENV
+echo "DATE=$(date "+%Y年%m月%d日%H时%M分")" >> $GITHUB_ENV
+echo "DATE1=$(date "+%Y.%m.%d")" >> $GITHUB_ENV
